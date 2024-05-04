bb2f3095ca372e317910599bfb540fea92a8078f
Murugesan Karthika <pritesmk@UMICH.EDU>
2024-05-03 23:25:55 -0400

    first tests

--- hedgemaster.py
+++ hedgemaster.py
@@ -1,17 +1,18 @@
 import pandas as pd
 
-df = pd.read_csv('your_file.csv')  
+df = pd.read_csv('data.csv')  
 
 y = 100
 
-df['x'] = y * (100 / (-df['udogsodds'])) / (df['favodds'] / 100)
+df['x'] = (y * (100 / (-df['favodds'])) + y) / (df['udogsodds'] / 100)
+print(df['x'])
 
-df['giftreward'] = df['x'] * (df['favodds'] / 100) - y
+df['giftreward'] = df['x'] * (df['udogsodds'] / 100) - y
 
 df['standardized_giftreward'] = df['giftreward'] / df['x']
 df_sorted_by_standardized = df.sort_values(by='standardized_giftreward', ascending=False)
 
-top_10_standardized = df_sorted_by_standardized.head(10)
+top_10_standardized = df_sorted_by_standardized
 
-output_with_standardized = top_10_standardized[['betname', 'favodds', 'udogsodds', 'standardized_giftreward']]
+output_with_standardized = top_10_standardized[['bname', 'favodds', 'udogsodds', 'giftreward', 'standardized_giftreward']]
 print(output_with_standardized)
